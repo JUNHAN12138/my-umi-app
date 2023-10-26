080737ed5ab5ad62a40840d18f123915709f6758
liangmingle <2206821639@qq.com>
2023-10-26 22:42:30 +0800

    feat: 发布到vercel

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
+  apiRoute: {
+    platform: "vercel",
+  },
   routes: [
     { path: "/", component: "index" },
     { path: "/docs", component: "docs" },
   ],
-  npmClient: 'pnpm',
+  npmClient: "pnpm",
 });
